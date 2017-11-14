8cadfa741cb71f1c36174beea3d2b6c9f2e5f8a3
Chris Watson <chris@watzon.me>
2017-11-14 11:26:01 -0700

    Remove some test code

--- src/synaptic.cr
+++ src/synaptic.cr
@@ -3,14 +3,3 @@ require "./synaptic/*"
 module Synaptic
   # TODO Put your code here
 end
-
-input = Synaptic::Layer.new(2)
-hidden = Synaptic::Layer.new(3)
-output = Synaptic::Layer.new(1)
-
-input.activate!
-input.propagate!(0.2)
-# hidden.activate!
-# output.activate!
-
-pp input
